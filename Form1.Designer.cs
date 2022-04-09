b8e0de7a0acc7f48167be701ebf3453681b5fae8
KaplyaKode <illyakapyshin@gmail.com>
2022-04-09 15:13:21 +0300

    1-5 labs

---
+++ Form1.Designer.cs
@@ -0,0 +1,477 @@
+﻿
+namespace _02
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.creationPanel = new System.Windows.Forms.Panel();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.personalizationPanel = new System.Windows.Forms.Panel();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label5 = new System.Windows.Forms.Label();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label4 = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.creationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.personalizationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.creationPanel.SuspendLayout();
+			this.panel1.SuspendLayout();
+			this.groupBox1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.personalizationPanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// creationPanel
+			// 
+			this.creationPanel.BackColor = System.Drawing.Color.Transparent;
+			this.creationPanel.Controls.Add(this.panel1);
+			this.creationPanel.Controls.Add(this.listBox1);
+			this.creationPanel.Controls.Add(this.panel2);
+			this.creationPanel.Location = new System.Drawing.Point(0, 35);
+			this.creationPanel.Name = "creationPanel";
+			this.creationPanel.Size = new System.Drawing.Size(626, 327);
+			this.creationPanel.TabIndex = 5;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.PaleTurquoise;
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.groupBox1);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.button1);
+			this.panel1.Controls.Add(this.checkBox1);
+			this.panel1.Location = new System.Drawing.Point(7, 3);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(197, 318);
+			this.panel1.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(5, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(137, 24);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Create account";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton3);
+			this.groupBox1.Location = new System.Drawing.Point(3, 137);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(191, 132);
+			this.groupBox1.TabIndex = 8;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Position";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.Location = new System.Drawing.Point(6, 28);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(87, 28);
+			this.radioButton1.TabIndex = 1;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Loader";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(6, 62);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(71, 28);
+			this.radioButton2.TabIndex = 5;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Clerk";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(6, 96);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(103, 28);
+			this.radioButton3.TabIndex = 6;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "Manager";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(7, 71);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(172, 29);
+			this.textBox2.TabIndex = 7;
+			this.textBox2.Text = "Password";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(7, 36);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(172, 29);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "Name";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(31, 269);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(133, 37);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Create";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(7, 106);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(84, 28);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "Admin";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// listBox1
+			// 
+			this.listBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 24;
+			this.listBox1.Location = new System.Drawing.Point(416, 3);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(201, 316);
+			this.listBox1.TabIndex = 5;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.PaleTurquoise;
+			this.panel2.Controls.Add(this.button2);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.checkedListBox1);
+			this.panel2.Location = new System.Drawing.Point(210, 3);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(200, 318);
+			this.panel2.TabIndex = 6;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(3, 137);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(132, 33);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Submit";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(132, 24);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Select position";
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.CheckOnClick = true;
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Items.AddRange(new object[] {
+            "Loader",
+            "Clerk",
+            "Manager"});
+			this.checkedListBox1.Location = new System.Drawing.Point(3, 33);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(132, 100);
+			this.checkedListBox1.TabIndex = 2;
+			// 
+			// personalizationPanel
+			// 
+			this.personalizationPanel.BackColor = System.Drawing.Color.Transparent;
+			this.personalizationPanel.Controls.Add(this.numericUpDown1);
+			this.personalizationPanel.Controls.Add(this.label5);
+			this.personalizationPanel.Controls.Add(this.trackBar1);
+			this.personalizationPanel.Controls.Add(this.label4);
+			this.personalizationPanel.Controls.Add(this.progressBar1);
+			this.personalizationPanel.Controls.Add(this.button3);
+			this.personalizationPanel.Controls.Add(this.label3);
+			this.personalizationPanel.Controls.Add(this.comboBox1);
+			this.personalizationPanel.Location = new System.Drawing.Point(3, 32);
+			this.personalizationPanel.Name = "personalizationPanel";
+			this.personalizationPanel.Size = new System.Drawing.Size(499, 327);
+			this.personalizationPanel.TabIndex = 6;
+			this.personalizationPanel.Visible = false;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.numericUpDown1.Enabled = false;
+			this.numericUpDown1.Location = new System.Drawing.Point(136, 204);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(47, 25);
+			this.numericUpDown1.TabIndex = 16;
+			this.numericUpDown1.Visible = false;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 284);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(132, 24);
+			this.label5.TabIndex = 15;
+			this.label5.Text = "Download limit";
+			this.label5.Visible = false;
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(12, 236);
+			this.trackBar1.Maximum = 1000;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(403, 45);
+			this.trackBar1.TabIndex = 7;
+			this.trackBar1.Value = 1000;
+			this.trackBar1.Visible = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 203);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(0, 24);
+			this.label4.TabIndex = 14;
+			this.label4.Visible = false;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 168);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(197, 23);
+			this.progressBar1.TabIndex = 13;
+			this.progressBar1.Visible = false;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 73);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(197, 89);
+			this.button3.TabIndex = 12;
+			this.button3.Text = "Choose folder to dowload to cloud";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Visible = false;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(8, 8);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(148, 24);
+			this.label3.TabIndex = 11;
+			this.label3.Text = "Choose account";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 35);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(197, 32);
+			this.comboBox1.TabIndex = 10;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.creationToolStripMenuItem,
+            this.personalizationToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(917, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// creationToolStripMenuItem
+			// 
+			this.creationToolStripMenuItem.Name = "creationToolStripMenuItem";
+			this.creationToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+			this.creationToolStripMenuItem.Text = "Creation";
+			this.creationToolStripMenuItem.Click += new System.EventHandler(this.creationToolStripMenuItem_Click);
+			// 
+			// personalizationToolStripMenuItem
+			// 
+			this.personalizationToolStripMenuItem.Name = "personalizationToolStripMenuItem";
+			this.personalizationToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+			this.personalizationToolStripMenuItem.Text = "Personalization";
+			this.personalizationToolStripMenuItem.Click += new System.EventHandler(this.personalizationToolStripMenuItem_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 368);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(87, 65);
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.BackColor = System.Drawing.Color.Transparent;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(78, 384);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(153, 20);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "Lombard corporated";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.BackColor = System.Drawing.Color.Transparent;
+			this.label7.Location = new System.Drawing.Point(721, 398);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(60, 24);
+			this.label7.TabIndex = 9;
+			this.label7.Text = "label7";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = global::_02.Properties.Resources._1953886;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(917, 431);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.creationPanel);
+			this.Controls.Add(this.menuStrip1);
+			this.Controls.Add(this.personalizationPanel);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "PopShop v2.0";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.creationPanel.ResumeLayout(false);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.personalizationPanel.ResumeLayout(false);
+			this.personalizationPanel.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Panel creationPanel;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Panel personalizationPanel;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem creationToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem personalizationToolStripMenuItem;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _02
+{
+	public partial class Form1 : Form
+	{
+		public class Account 
+		{
+			string name;
+			string password;
+			bool isAdmin;
+			string position;
+			//string path;
+
+			public Account(string Name, string Password, bool IsAdmin, string Position, string Path = "") 
+			{
+				name = Name;
+				password = Password;
+				isAdmin = IsAdmin;
+				position = Position;
+			}
+
+			/*public void SetPicture(string Path)
+			{
+				this.path = Path;
+			}*/
+			public string GetName() 
+			{
+				return this.name;
+			}
+			public string GetPosition()
+			{
+				return this.position;
+			}
+		}
+
+		List<Account> accounts = new List<Account>();
+		int timer = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			pictureBox1.Image = Image.FromFile("D:\\Programing\\C# Forms\\02\\av.gif");
+			checkedListBox1.SetItemChecked(0, true);
+			checkedListBox1.SetItemChecked(1, true);
+			checkedListBox1.SetItemChecked(2, true);
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string position;
+
+			foreach (var account in accounts)
+				if (textBox1.Text == account.GetName())
+				{
+					MessageBox.Show("This name is engaged!");
+					return;
+				}
+
+
+			if (radioButton1.Checked)
+				position = radioButton1.Text;
+			else if (radioButton2.Checked)
+				position = radioButton2.Text;
+			else 
+				position = radioButton3.Text;
+
+			Account newAccount = new Account(textBox1.Text, textBox2.Text, checkBox1.Checked, position);
+
+
+			accounts.Add(newAccount);
+
+			listBox1.Items.Add(textBox1.Text);
+			comboBox1.Items.Add(newAccount.GetName());
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			listBox1.Items.Clear();
+
+			foreach (string item in checkedListBox1.CheckedItems)
+			{
+				if (item == "Loader")
+				{
+					foreach (var account in accounts)
+						if (checkedListBox1.Items[0].ToString() == account.GetPosition())
+							listBox1.Items.Add(account.GetName());
+
+				} 
+				if (item == "Clerk")
+				{
+					foreach (var account in accounts)
+						if (checkedListBox1.Items[1].ToString() == account.GetPosition())
+							listBox1.Items.Add(account.GetName());
+
+				}
+				if (item == "Manager")
+				{
+					foreach (var account in accounts)
+						if (checkedListBox1.Items[2].ToString() == account.GetPosition())
+							listBox1.Items.Add(account.GetName());
+
+				}
+			}
+		}
+
+		async private void button3_Click(object sender, EventArgs e)
+		{
+			if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+			{
+				string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+				int qt = files.Length;
+				label4.Visible = true;
+				progressBar1.Visible = true;
+				trackBar1.Visible = true;
+				label5.Visible = true;
+				numericUpDown1.Visible = true;
+				progressBar1.Maximum = qt;
+				progressBar1.Value = 0;
+
+				if (qt == 0)
+				{
+					MessageBox.Show("Files not found!");
+					return;
+				}
+
+				label4.Text = "Count of files:";
+
+				while (qt > 0)
+				{
+					numericUpDown1.Value = qt - 1;
+					progressBar1.Value++;
+					qt--;
+					await Task.Delay(trackBar1.Value);
+				}
+
+				MessageBox.Show("All files downloaded to cloud");
+			}
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			button3.Visible = true;
+		}
+
+		private void creationToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			foreach  (Panel panel in Controls.OfType<Panel>())
+				panel.Visible = false;
+
+			creationPanel.Visible = true;
+		}
+
+		private void personalizationToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			foreach (Panel panel in Controls.OfType<Panel>())
+				panel.Visible = false;
+
+			personalizationPanel.Visible = true;
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			timer++;
+			label7.Text = $"Session time: {(timer / 3600)%60}:{(timer / 60)%60}:{timer%60}         ";
+		}
+	}
+}
